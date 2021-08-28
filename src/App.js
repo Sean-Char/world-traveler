b367084cba1f63b6327dc2fd3ed6632185160c8b
Sean Charoutanh <scharoutanh@gmail.com>
2021-08-28 15:03:21 -0600

    scaffold functional component for created component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map.jsx';
+
 
 const App = () => {
     return (
-        <div>
-            <h1>Get to da Chopa!</h1>
-        </div>
-    )
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100' }}>
+                <Grid xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </>
+    );
 }
 
 export default App;
