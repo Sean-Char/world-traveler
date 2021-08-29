8e5c9a26142432fca71feec997cff4337787bae9
Sean Charoutanh <scharoutanh@gmail.com>
2021-08-29 12:20:18 -0600

    Revert "Revert "Merge pull request #5 from Sean-Char/work-branch""
    This reverts commit 876a66cc87eb209cbcfc540469cd239c65b3e5d6.

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
+const rapidApi = process.env.REACT_APP_RAPID_API_KEY
+
 export const getPlacesData = async (sw, ne) => {
     try {
         const { data: { data } } = await axios.get(URL, {
@@ -13,7 +15,8 @@ export const getPlacesData = async (sw, ne) => {
             },
             headers: {
               'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-              'x-rapidapi-key': ''
+              'x-rapidapi-key': rapidApi
+
             }
           });
 
